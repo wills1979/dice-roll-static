534ec23726303f7749f874d39a72b76cb7aa9841
wills1979 <107662428+wills1979@users.noreply.github.com>
2025-04-21 20:17:48 +0000

    hello world

---
+++ app/controllers/dice_controller.rb
@@ -0,0 +1,7 @@
+class DiceController < ApplicationController
+  
+  def two_six
+    render({ :template => "dice_templates/2d6" })
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
+  get("/dice/2/6", { :controller => "dice", :action => "two_six"})
 
-  # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
-
-  # get "/your_first_screen" => "pages#first"
-  
 end
