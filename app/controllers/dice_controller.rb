c897810d7b6b8bf091df1e20518dffcb2fbbeee1
wills1979 <107662428+wills1979@users.noreply.github.com>
2025-04-21 20:37:22 +0000

    finished app

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -1,7 +1,66 @@
 class DiceController < ApplicationController
   
-  def two_six
-    render({ :template => "dice_templates/2d6" })
+  def home 
+
+    render({ :template => "dice_templates/home" })
+
   end
+
+  def two_six
+    @rolls = []
+
+    @num_dice = 2
+    @num_sides = 6
+
+    @num_dice.times do
+      die = rand(1..@num_sides)
+      @rolls.push(die)
+    end
+
+    render({ :template => "dice_templates/dice_d_sides" })
+  end
+
+  def two_ten
+    @rolls = []
+
+    @num_dice = 2
+    @num_sides = 10
+
+    @num_dice.times do
+      die = rand(1..@num_sides)
+      @rolls.push(die)
+    end
+
+    render({ :template => "dice_templates/dice_d_sides" })
+  end
+
+  def one_twenty
+    @rolls = []
+
+    @num_dice = 1
+    @num_sides = 20
+
+    @num_dice.times do
+      die = rand(1..@num_sides)
+      @rolls.push(die)
+    end
+
+    render({ :template => "dice_templates/dice_d_sides" })
+  end
+
+  def five_four
+    @rolls = []
+
+    @num_dice = 5
+    @num_sides = 4
+
+    @num_dice.times do
+      die = rand(1..@num_sides)
+      @rolls.push(die)
+    end
+
+    render({ :template => "dice_templates/dice_d_sides" })
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+  get("/", { :controller => "dice", :action => "home"})
+
   get("/dice/2/6", { :controller => "dice", :action => "two_six"})
+
+  get("/dice/2/10", { :controller => "dice", :action => "two_ten"})
+
+  get("/dice/1/20", { :controller => "dice", :action => "one_twenty"})
+
+  get("/dice/5/4", { :controller => "dice", :action => "five_four"})
 
 end
